0a5478cda2f5c69a8c708eb5cb53e2e698ab1ab7
Dor <dorzairi@ymail.com>
2021-03-24 11:23:50 -0400

    init + task 1 done

---
+++ Python Function Lab.py
@@ -0,0 +1,12 @@
+# task 1
+def pokemon_contains(incoming_letter):
+	if incoming_letter in "pokemon":
+		return True
+	else:
+		return False
+
+result1 = pokemon_contains("k")
+print(result1)
+
+result1 = pokemon_contains("o")
+print(result1)
