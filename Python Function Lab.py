69839081c752cbcbdf3e1e428d277213c17525bc
Dor <dorzairi@ymail.com>
2021-03-24 11:37:51 -0400

    Task 3 done

--- Python Function Lab.py
+++ Python Function Lab.py
@@ -12,14 +12,24 @@
 # print(result1)
 
 # task 2
-a = 1
-b = 3
+# a = 1
+# b = 3
+#
+# def sum_two(a,b):
+# 	answer = a + b
+# 	return answer
+#
+# result3 = sum_two(a,b) # a and b were not defined
+# print(result3)
+# result4 = sum_two(5,6)
+# print(result4)
 
-def sum_two(a,b):
-	answer = a + b
-	return answer
+# task 3
 
-result3 = sum_two(a,b) # a and b were not defined
-print(result3)
-result4 = sum_two(5,6)
-print(result4)
+def multiply(num1 , num2):
+	answer = num1*num2 # a and b were not an argument in the function but num1 and num2 were
+	return answer  #missing return answer
+result5 = multiply( 10 , 10 )
+print(result5)
+result6 = multiply( 5 , 6 )
+print(result6)
