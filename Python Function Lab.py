6d2ac027921c36c2a03ff8a9bb350a6dcc458164
Dor <dorzairi@ymail.com>
2021-03-24 11:28:11 -0400

    task 2 completed

--- Python Function Lab.py
+++ Python Function Lab.py
@@ -1,12 +1,25 @@
 # task 1
-def pokemon_contains(incoming_letter):
-	if incoming_letter in "pokemon":
-		return True
-	else:
-		return False
+# def pokemon_contains(incoming_letter):
+# 	if incoming_letter in "pokemon":
+# 		return True
+# 	else:
+# 		return False
+#
+# result1 = pokemon_contains("k")
+# print(result1)
+#
+# result1 = pokemon_contains("o")
+# print(result1)
 
-result1 = pokemon_contains("k")
-print(result1)
+# task 2
+a = 1
+b = 3
 
-result1 = pokemon_contains("o")
-print(result1)
+def sum_two(a,b):
+	answer = a + b
+	return answer
+
+result3 = sum_two(a,b) # a and b were not defined
+print(result3)
+result4 = sum_two(5,6)
+print(result4)
